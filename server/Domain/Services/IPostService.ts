edc02f9cd3d86614b4ce0776466edf77c8e552cc
vadimdom <vadimchernyi2@gmail.com>
2018-08-14 17:31:05 +0300

    lazy-load

--- server/Domain/Services/IPostService.ts
+++ server/Domain/Services/IPostService.ts
@@ -1,7 +1,7 @@
 import PostEntity from '../../Data/Entities/PostEntity';
 
 export interface IPostService {
-  getPosts(): Promise<PostEntity[]>;
+  getPosts(count: string, offset: string): Promise<PostEntity[]>;
   addPost(data: any): Promise<PostEntity>;
   updatePost(data: any): Promise<PostEntity>;
   deletePost(id: number): Promise<PostEntity[]>;
--- server/Domain/Services/Impl/PostService.ts
+++ server/Domain/Services/Impl/PostService.ts
@@ -10,8 +10,18 @@ export class PostService implements IPostService {
     this._postRepository = postRepository;
   }
 
-  public async getPosts(): Promise<PostEntity[]> {
-    return this._postRepository.findAll({});
+  public async getPosts(countStr: string, offsetStr: string): Promise<PostEntity[]> {
+    const options = {};
+    const count = parseInt(countStr, 10);
+    const offset = parseInt(offsetStr, 10);
+
+    if (!isNaN(count)) {
+      options.count = count;
+    }
+    if (!isNaN(offset)) {
+      options.offset = offset;
+    }
+    return this._postRepository.findAll({ limit: options.count, offset: options.offset });
   }
 
   public async addPost(data: any): Promise<PostEntity> {
